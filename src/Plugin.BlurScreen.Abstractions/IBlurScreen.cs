2ce149f7d37f62db51c0791b1c54e6f8f2e54c58
bohdan.hrybach <bohdan.hrybach@gmail.com>
2018-10-26 16:48:27 +0300

    Add Plugin.BlurScreen.Abstractions

---
+++ src/Plugin.BlurScreen.Abstractions/IBlurScreen.cs
@@ -0,0 +1,18 @@
+﻿namespace Plugin.BlurScreen.Abstractions
+{
+    /// <summary>
+    /// Interface for BlurScreen
+    /// </summary>
+    public interface IBlurScreen
+    {
+        /// <summary>
+        /// Blurs entire screen
+        /// </summary>
+        void Blur();
+
+        /// <summary>
+        /// Unblurs entire screen
+        /// </summary>
+        void Unblur();
+    }
+}
