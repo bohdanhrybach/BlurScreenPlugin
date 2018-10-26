4c47298afb69a6ed9ffa66048745bddc5f781e33
bohdan.hrybach <bohdan.hrybach@gmail.com>
2018-10-26 18:17:26 +0300

    Add Plugin.BlurScreen.Android

---
+++ src/Plugin.BlurScreen.Android/BlurFragmentDialog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Android.Graphics.Drawables;
+using Plugin.CurrentActivity;
+
+namespace Plugin.BlurScreen
+{
+    public class BlurFragmentDialog : Android.Support.V4.App.DialogFragment
+    {
+        public override void OnStart()
+        {
+            base.OnStart();
+
+            var blurredScreenBitmap = BlurUtility.GetBlurrecScreen(CrossCurrentActivity.Current.Activity);
+            var draw = new BitmapDrawable(Resources, blurredScreenBitmap);
+            Dialog.Window.SetBackgroundDrawable(draw);
+        }
+    }
+}
---
+++ src/Plugin.BlurScreen.Android/BlurScreenImplementation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.Support.V7.App;
+using Plugin.BlurScreen.Abstractions;
+using Plugin.CurrentActivity;
+
+namespace Plugin.BlurScreen
+{
+    public class BlurScreenImplementation : IBlurScreen
+    {
+        public void Blur()
+        {
+            _blurredDialog = new BlurFragmentDialog();
+
+            var activity = (AppCompatActivity)CrossCurrentActivity.Current.Activity;
+            _blurredDialog.Show(activity.SupportFragmentManager, nameof(BlurFragmentDialog));
+        }
+
+        public void Unblur()
+        {
+            _blurredDialog.Dismiss();
+            _blurredDialog.Dispose();
+            _blurredDialog = null;
+        }
+
+        private BlurFragmentDialog _blurredDialog;
+    }
+}
