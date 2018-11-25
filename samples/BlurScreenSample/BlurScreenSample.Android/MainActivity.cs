4205408533dbac77cf5079178f7505a44b529684
bohdan.hrybach <bohdan.hrybach@gmail.com>
2018-11-25 20:21:13 +0200

    Add BlurScreenSample project

---
+++ samples/BlurScreenSample/BlurScreenSample.Android/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Plugin.CurrentActivity;
+
+namespace BlurScreenSample.Droid
+{
+    [Activity(Label = "BlurScreenSample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ samples/BlurScreenSample/BlurScreenSample/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Plugin.BlurScreen;
+using System.Threading.Tasks;
+
+namespace BlurScreenSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void Handle_BlurScreenPressed(object sender, EventArgs e)
+        {
+            CrossBlurScreen.Current.Blur();
+
+            await ShowAlert();
+
+            CrossBlurScreen.Current.Unblur();
+        }
+
+        private async Task ShowAlert() 
+        {
+            // NOTE: Some trouble when showing alert on Android
+            //       Probably dialog shows quicker than screen blurs
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                await Task.Delay(1000);
+            }
+
+            await DisplayAlert("BlurScreenSample", "Press Ok to unblur screen", "Ok");
+        }
+    }
+}
